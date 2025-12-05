9da44b616a0da789b11db0ed301962741c651908
Stossy11 <stossy11@stossy11.com>
2025-12-06 09:27:01 +1100

    Change to StosSign lib

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "StosSign",
+            name: "StosSignLib",
             targets: ["StosOpenSSL", "StosCSRP","StosSign"]),
     ],
     dependencies: [
