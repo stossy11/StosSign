a52dfb46b0748ac32fab94f1512797231902e3d5
Stossy11 <stossy11@stossy11.com>
2025-12-06 17:52:09 +1100

    revert fix certificate

--- Sources/StosSign/Models/Apple API/Certificate.swift
+++ Sources/StosSign/Models/Apple API/Certificate.swift
@@ -77,7 +77,7 @@ public final class Certificate {
         let certificateData = Self.extractCertificateData(from: attributes)
         let machineName = Self.extractString(attributes["machineName"])
         let machineIdentifier = Self.extractString(attributes["machineId"])
-        let identifier = Self.extractString(response["responseId"]) // seems to be responseId instead of id like AltSign had :shrug:
+        let identifier = Self.extractString(response["id"])
         
         if let data = certificateData, let certificate = Certificate(certificateData: data) {
             self.init(
