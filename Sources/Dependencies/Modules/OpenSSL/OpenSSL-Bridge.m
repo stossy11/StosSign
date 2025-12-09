9306325c871ce34f4f135e47f807c7c7dcbf36cd
Stossy11 <stossy11@stossy11.com>
2025-12-09 11:27:55 +1100

    Fix generating CSR with updated OpenSSL

--- Sources/Dependencies/Modules/OpenSSL/OpenSSL-Bridge.m
+++ Sources/Dependencies/Modules/OpenSSL/OpenSSL-Bridge.m
@@ -256,96 +256,51 @@ cleanup:
 }
 
 int generate_certificate_request(unsigned char **outputRequest, long *requestLength,
-                                 unsigned char **outputPrivateKey, long *privateKeyLength) {
-    BIGNUM *bignum = NULL;
-    RSA *rsa = NULL;
+                                unsigned char **outputPrivateKey, long *privateKeyLength) {
+    EVP_PKEY *pkey = NULL;
     X509_REQ *request = NULL;
-    EVP_PKEY *publicKey = NULL;
-    BIO *csr = NULL;
-    BIO *privateKey = NULL;
-
+    BIO *csrBio = NULL;
+    BIO *keyBio = NULL;
     int success = 0;
 
-    bignum = BN_new();
-    if (bignum == NULL || BN_set_word(bignum, RSA_F4) != 1) {
-        goto cleanup;
-    }
+    pkey = EVP_PKEY_Q_keygen(NULL, NULL, "RSA", (size_t)2048);
+    if (pkey == NULL) goto cleanup;
 
-    rsa = RSA_new();
-    if (rsa == NULL || RSA_generate_key_ex(rsa, 2048, bignum, NULL) != 1) {
-        goto cleanup;
-    }
-
-    /* Generate request */
     request = X509_REQ_new();
-    if (request == NULL || X509_REQ_set_version(request, 1) != 1) {
-        goto cleanup;
-    }
-
-    const char *country = "US";
-    const char *state = "CA";
-    const char *city = "Los Angeles";
-    const char *organization = "StosSign";
-    const char *commonName = "StosSign";
+    if (request == NULL || X509_REQ_set_version(request, 0) != 1) goto cleanup;
 
     X509_NAME *subject = X509_REQ_get_subject_name(request);
-    if (subject == NULL) {
-        goto cleanup;
-    }
+    X509_NAME_add_entry_by_txt(subject, "C",  MBSTRING_ASC, (unsigned char *)"US", -1, -1, 0);
+    X509_NAME_add_entry_by_txt(subject, "ST", MBSTRING_ASC, (unsigned char *)"CA", -1, -1, 0);
+    X509_NAME_add_entry_by_txt(subject, "L",  MBSTRING_ASC, (unsigned char *)"Los Angeles", -1, -1, 0);
+    X509_NAME_add_entry_by_txt(subject, "O",  MBSTRING_ASC, (unsigned char *)"StosSign", -1, -1, 0);
+    X509_NAME_add_entry_by_txt(subject, "CN", MBSTRING_ASC, (unsigned char *)"StosSign", -1, -1, 0);
 
-    X509_NAME_add_entry_by_txt(subject, "C", MBSTRING_ASC, (const unsigned char *)country, -1, -1, 0);
-    X509_NAME_add_entry_by_txt(subject, "ST", MBSTRING_ASC, (const unsigned char *)state, -1, -1, 0);
-    X509_NAME_add_entry_by_txt(subject, "L", MBSTRING_ASC, (const unsigned char *)city, -1, -1, 0);
-    X509_NAME_add_entry_by_txt(subject, "O", MBSTRING_ASC, (const unsigned char *)organization, -1, -1, 0);
-    X509_NAME_add_entry_by_txt(subject, "CN", MBSTRING_ASC, (const unsigned char *)commonName, -1, -1, 0);
+    if (X509_REQ_set_pubkey(request, pkey) != 1) goto cleanup;
 
-    publicKey = EVP_PKEY_new();
-    if (publicKey == NULL || EVP_PKEY_assign_RSA(publicKey, rsa) != 1) {
-        goto cleanup;
-    }
+    if (X509_REQ_sign(request, pkey, EVP_sha256()) <= 0) goto cleanup;
 
-    if (X509_REQ_set_pubkey(request, publicKey) != 1) {
-        goto cleanup;
-    }
+    csrBio = BIO_new(BIO_s_mem());
+    if (PEM_write_bio_X509_REQ(csrBio, request) != 1) goto cleanup;
 
-    if (X509_REQ_sign(request, publicKey, EVP_sha256()) <= 0) {
-        goto cleanup;
-    }
+    keyBio = BIO_new(BIO_s_mem());
+    if (PEM_write_bio_PrivateKey(keyBio, pkey, NULL, NULL, 0, NULL, NULL) != 1) goto cleanup;
 
-    csr = BIO_new(BIO_s_mem());
-    if (csr == NULL || PEM_write_bio_X509_REQ(csr, request) != 1) {
-        goto cleanup;
-    }
-
-    privateKey = BIO_new(BIO_s_mem());
-    if (privateKey == NULL || PEM_write_bio_RSAPrivateKey(privateKey, rsa, NULL, NULL, 0, NULL, NULL) != 1) {
-        goto cleanup;
-    }
-
-    /* Extract Data */
-    char *csrData = NULL;
-    *requestLength = BIO_get_mem_data(csr, &csrData);
+    char *data = NULL;
+    *requestLength = BIO_get_mem_data(csrBio, &data);
     *outputRequest = (unsigned char *)malloc(*requestLength);
-    memcpy(*outputRequest, csrData, *requestLength);
+    memcpy(*outputRequest, data, *requestLength);
 
-    char *privateKeyData = NULL;
-    *privateKeyLength = BIO_get_mem_data(privateKey, &privateKeyData);
+    *privateKeyLength = BIO_get_mem_data(keyBio, &data);
     *outputPrivateKey = (unsigned char *)malloc(*privateKeyLength);
-    memcpy(*outputPrivateKey, privateKeyData, *privateKeyLength);
+    memcpy(*outputPrivateKey, data, *privateKeyLength);
 
     success = 1;
 
 cleanup:
-    if (publicKey != NULL) {
-        EVP_PKEY_free(publicKey);
-    } else {
-        RSA_free(rsa);
-    }
-
-    BN_free(bignum);
     X509_REQ_free(request);
-    BIO_free_all(csr);
-    BIO_free_all(privateKey);
-
+    EVP_PKEY_free(pkey);
+    BIO_free_all(csrBio);
+    BIO_free_all(keyBio);
     return success;
 }
