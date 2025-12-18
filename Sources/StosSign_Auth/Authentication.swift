11359b8f319b8b78493a2a67dc13d8344e615302
Stossy11 <stossy11@stossy11.com>
2025-12-18 23:05:30 +1100

    Add typealias

---
+++ Sources/StosSign_Auth/Authentication.swift
@@ -0,0 +1,26 @@
+//
+//  Authentication.swift
+//  StosSign
+//
+//  Created by Stossy11 on 18/12/2025.
+//
+
+import Foundation
+import StosSign_API
+
+public typealias AnisetteData = StosSign_API.AnisetteData
+public typealias Account = StosSign_API.Account
+public typealias AppleAPISession = StosSign_API.AppleAPISession
+public typealias AppleAPIError = StosSign_API.AppleAPIError
+
+
+final class Authentication {
+    public func authenticate(
+        appleID unsanitizedAppleID: String,
+        password: String,
+        anisetteData: AnisetteData,
+        verificationHandler: ((@escaping (String?) -> Void) async -> Void)? = nil
+    ) async throws -> (Account, AppleAPISession) {
+        try await AppleAPI.shared.authenticate(appleID: unsanitizedAppleID, password: password, anisetteData: anisetteData, verificationHandler: verificationHandler)
+    }
+}
